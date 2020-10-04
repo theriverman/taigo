f45821d13ae2e636223c715d6a688c4b9e8cce74
Kristof Daja <kristof.daja@semcon.com>
2020-10-04 16:29:12 +0200

    exemplary env files added to ./tests

---
+++ tests/.env.bash
@@ -0,0 +1,4 @@
+export env_ServerURL='https://api.taiga.io'
+export env_ServerUsername='your-taiga-username'
+export env_ServerPassword='your-taiga-password'
+export env_DummyProjectID='your-taiga-project-id'
---
+++ tests/.env.ps1
@@ -0,0 +1,4 @@
+$env:env_ServerURL      = 'https://api.taiga.io'
+$env:env_ServerUsername = 'your-taiga-username'
+$env:env_ServerPassword = 'your-taiga-password'
+$env:env_DummyProjectID = 'your-taiga-project-id'
