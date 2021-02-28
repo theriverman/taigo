0cd18080d86c502f28819088c62c99c6f546a362
Kristof Daja <kristof@daja.hu>
2021-02-28 10:35:39 +0100

    Improved the build-and-run.sh script with env vars

--- contribute/build-and-run.sh
+++ contribute/build-and-run.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-clear
-go build -ldflags "\
--X main.taigaUsername=your-username \
--X main.taigaPassword=your-password \
--X main.sandboxProjectSlug=my-sandbox-slug \
--X main.sandboxEpicID=1234567890 \
--X main.sandboxFileUploadPath=/tmp/bad-puns-make-me-sic.png\
+set -e
+
+go build -o taigo-dev -ldflags \
 " \
--o taigo-dev.exe || ECHO Failed to build the binary && exit 1
+  -X main.taigaUsername=$taigaUsername \
+  -X main.taigaPassword=$taigaPassword \
+  -X main.sandboxProjectSlug=$sandboxProjectSlug \
+  -X main.sandboxEpicID=$sandboxEpicID \
+  -X main.sandboxFileUploadPath=$sandboxFileUploadPath \
+"
 
 ./taigo-dev
