8160325abbbc4fd2b2ffe3a17518169e878a8533
Kristof Daja <kristof.daja@semcon.com>
2021-02-28 10:29:50 +0100

    Changed windows test script from .bat to ps1

--- contribute/build-and-run.bat
+++
@@ -1,5 +0,0 @@
-@ECHO off
-
-cls
-go build -ldflags "-X main.taigaUsername=my-username -X main.taigaPassword=my-password -X main.sandboxProjectSlug=my-sandbox-slug -X main.sandboxEpicID=1234567890 -X main.sandboxFileUploadPath=C:\tmp\bad-puns-make-me-sic.png" -o taigo-dev.exe || ECHO Failed to build the binary && exit 1
-.\taigo-dev.exe
