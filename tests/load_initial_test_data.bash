7c1aaa171a68ffc1bca9b3b2a51ffcd0437c2192
Kristof Daja <kristof.daja@semcon.com>
2021-02-28 18:48:09 +0100

    Configuring...

--- tests/load_initial_test_data.bash
+++ tests/load_initial_test_data.bash
@@ -1,2 +1,16 @@
-docker cp initial_test_data.json taiga-docker_taiga-back_1:/taiga-back/media/initial_test_data.json
-docker-compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage loaddata /taiga-back/media/initial_test_data.json
+# Wait for automatic migrations to apply after executing `docker-compose up -d`
+
+while :
+do
+	curl -X GET -s --fail -o /dev/null http://localhost:9000/api/v1/stats/discover
+	if [[ "$?" != '0' ]]; then
+		sleep 1
+		continue
+	fi
+	break
+done
+
+echo "Taiga is up!"
+
+docker cp initial_test_data.json taiga-docker_taiga-back_1:/taiga-back/media/initial_test_data.json  || exit 1
+docker-compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage loaddata /taiga-back/media/initial_test_data.json || exit 1
